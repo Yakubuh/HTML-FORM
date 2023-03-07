a1c4a7dd76ac77382a2d7c4ca8315af005cfa478
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2023-03-07 14:10:07 +0000

    Create form.js

---
+++ form.js
@@ -0,0 +1,24 @@
+function myname(){
+
+    if(document.myform.fname.value == null || document.myform.fname.value == "" )
+    {
+        alert("first name cannot be empty");
+            return false;
+    }
+        if(document.myform.lname.value == null ||  document.myform.lname.value =="")
+        {
+            alert("last name cannot be empty");
+            return false;
+    }
+    
+        
+    if(document.myform.gender.value==null){
+        alert("Select your gender");
+    }
+    else{
+        alert("Submitted");
+        document.location.reload(true);
+    }
+    
+    
+    }
